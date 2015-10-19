07e4a2ce994966717ee2dedd8bba308a856ea7c1
Yokoe <yokoe.hnut@gmail.com>
2015-10-19 13:03:39 +0900

    initial commit

---
+++ Chapter3-4/Chapter3-4/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Chapter3-4
+//
+//  Created by Yokoe on 10/19/15.
+//  Copyright © 2015 HANAUTA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool
+    {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
