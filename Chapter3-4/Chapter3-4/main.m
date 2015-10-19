5a75ccb45ddcdb44633e5f40a599181f5e628b56
Yokoe <yokoe.hnut@gmail.com>
2015-10-19 14:16:47 +0900

    Finish Chapter3-4

--- Chapter3-4/Chapter3-4/main.m
+++ Chapter3-4/Chapter3-4/main.m
@@ -8,12 +8,62 @@
 
 #import <Foundation/Foundation.h>
 
+@interface AClass : NSObject
+- (void)method1 ;
+- (void)method2 ;
+@end
+
+@implementation AClass
+- (void)method1
+{
+    NSLog(@"method1 of Class A\n") ;
+}
+
+- (void)method2
+{
+    NSLog(@"method2 of Class A\n") ;
+}
+@end
+
+@interface BClass : AClass
+- (void)method2 ;
+@end
+
+@implementation BClass
+- (void)method2
+{
+    NSLog(@"method2 of Class B\n") ;
+    NSLog(@"self----->") ;
+    [self method1] ;
+    NSLog(@"super----->") ;
+    [super method2] ;
+}
+@end
+
+@interface CClass : BClass
+- (void)method1 ;
+@end
+
+@implementation CClass
+- (void)method1
+{
+    NSLog(@"method1 of Class C\n") ;
+}
+@end
+
+/* メイン部 */
 int main(int argc, const char * argv[])
 {
     @autoreleasepool
     {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        BClass *bInstance = [[BClass alloc] init] ;
+        CClass *cInstance = [[CClass alloc] init] ;
+        NSLog(@"----- instance of B -----\n") ;
+        [bInstance method1] ;
+        [bInstance method2] ;
+        NSLog(@"----- instance of C -----\n") ;
+        [cInstance method1] ;
+        [cInstance method2] ;
     }
-    return 0;
+    return 0 ;
 }
